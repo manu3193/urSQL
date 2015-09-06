0d065e3b134bbf378975c1ea2dff1a139c42953c
majesco <nicolas.j2007@gmail.com>
2015-09-05 20:10:32 -0600

    Arranging
    Acomodando los modulos y  las clases

--- DatabaseRuntimeProcessor/src/main/java/CreateTable.java
+++ DatabaseRuntimeProcessor/src/main/java/CreateTable.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +13,11 @@
  */
 public class CreateTable {
     
+    private ArrayList<String> command;
+     
+    
+    private void crearPlanEjecucion() {
+   
+        
+     }    
 }
