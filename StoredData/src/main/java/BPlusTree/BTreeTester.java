cd55b84ae0e97a0fd2f6845b2475d6890e5f3575
majesco <nicolas.j2007@gmail.com>
2015-09-05 20:00:56 -0600

    Arranging
    Acomodando las clases y los modelos

--- StoredData/src/main/java/BPlusTree/BTreeTester.java
+++ StoredData/src/main/java/BPlusTree/BTreeTester.java
@@ -1,20 +1,14 @@
-
-import java.util.ArrayList;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package BPlusTree;
 
 /**
  *
  * @author nicolasjimenez
  */
-public class GetStatus {
-    
-    
-    private ArrayList<String> command;
-    
-    
+public class BTreeTester {
+
 }
