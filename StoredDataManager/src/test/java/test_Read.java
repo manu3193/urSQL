9c297042bcae9086680cc4241753c40359a5117c
manu3193 <manu_z3@hotmail.com>
2015-09-15 21:31:34 -0600

    pruebas listas

--- StoredDataManager/src/test/java/test_Read.java
+++ StoredDataManager/src/test/java/test_Read.java
@@ -3,57 +3,61 @@
  */
 import java.io.*;
 import DataBaseFile.*;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class test_Read {
 
-    public static void main( String args[]) {
+    InputStream inputStream;
+    DBFReader reader;
 
-        try {
-
-            // crea objeto DBF
-            //
-            InputStream inputStream  = new FileInputStream("StoredDataManager/target/test.db");
-            DBFReader reader = new DBFReader( inputStream);
-
-            // obtiene el numero de campos
-            //
-            int numberOfFields = reader.getFieldCount();
-
-            // bucle para iterar sobre todos los campos
-            //
-            for( int i=0; i<numberOfFields; i++) {
-
-                DBFField field = reader.getField( i);
-
-                // Realizar las operaciones requeridas
-                //
-                System.out.println( field.getName());
-            }
-
-            // empieza a leer las columnas
-            //
-            Object []rowObjects;
-
-            while( (rowObjects = reader.nextRecord()) != null) {
-
-                for( int i=0; i<rowObjects.length; i++) {
-
-                    System.out.println( rowObjects[i]);
-                }
-            }
-
-            // se han recorrido todas las filas
-
-            inputStream.close();
-        }
-        catch( DBFException e) {
-
-            System.out.println( e.getMessage());
-        }
-        catch( IOException e) {
-
-            System.out.println( e.getMessage());
-        }
+    @Before
+    public void init() throws IOException{
+        // crea objeto DBF
+        //
+         inputStream  = new FileInputStream("test.db");
+         reader = new DBFReader( inputStream);
     }
+
+    @Test
+    public void testFieldCount() throws IOException {
+        // obtiene el numero de campos
+        //
+        int numberOfFields = reader.getFieldCount();
+        assertEquals(numberOfFields,3);
+        // bucle para iterar sobre todos los campos
+        //
+        for( int i=0; i<numberOfFields; i++) {
+
+            DBFField field = reader.getField( i);
+
+            // Realizar las operaciones requeridas
+            //
+            System.out.println(field.getName());
+
+        }
+        // empieza a leer las columnas
+        //
+        Object []rowObjects;
+
+        while( (rowObjects = reader.nextRecord()) != null) {
+
+            for( int i=0; i<rowObjects.length; i++) {
+
+                System.out.println( rowObjects[i]);
+            }
+        }
+
+        // se han recorrido todas las filas
+
+        inputStream.close();
+    }
+
+
+
+
+
+
+
 }
 
--- StoredDataManager/src/test/java/test_Write.java
+++ StoredDataManager/src/test/java/test_Write.java
@@ -2,14 +2,23 @@
  * Created by manzumbado on 9/15/15.
  */
 import DataBaseFile.*;
-
+import org.junit.*;
 import java.io.*;
+
 
 public class test_Write {
 
-    public static void main( String args[])
-            throws DBFException, IOException {
+    DBFWriter writer;
+    FileOutputStream fos;
 
+    @Before
+    public void test_Write() throws FileNotFoundException{
+        writer = new DBFWriter();
+         fos = new FileOutputStream( "test.db");
+    }
+
+    @Test
+    public void populate() throws DBFException, IOException{
         // Creamos un arreglo de 3 campos
         //
         DBFField fields[] = new DBFField[ 3];
@@ -17,24 +26,19 @@ public class test_Write {
         fields[0] = new DBFField();
         fields[0].setName( "emp_code");
         fields[0].setDataType( DBFField.FIELD_TYPE_C);
-        fields[0].setFieldLength( 10);
+        fields[0].setFieldLength(10);
 
         fields[1] = new DBFField();
-        fields[1].setName( "emp_name");
+        fields[1].setName("emp_name");
         fields[1].setDataType( DBFField.FIELD_TYPE_C);
-        fields[1].setFieldLength( 20);
+        fields[1].setFieldLength(20);
 
         fields[2] = new DBFField();
-        fields[2].setName( "salary");
+        fields[2].setName("salary");
         fields[2].setDataType( DBFField.FIELD_TYPE_N);
-        fields[2].setFieldLength( 12);
-        fields[2].setDecimalCount( 2);
-
-        DBFWriter writer = new DBFWriter();
-        writer.setFields( fields);
-
-        // llenamos el DBWriter
-        //
+        fields[2].setFieldLength(12);
+        fields[2].setDecimalCount(2);
+        writer.setFields(fields);
 
         Object rowData[] = new Object[3];
         rowData[0] = "1000";
@@ -56,8 +60,6 @@ public class test_Write {
         rowData[2] = new Double( 7350.00);
 
         writer.addRecord( rowData);
-
-        FileOutputStream fos = new FileOutputStream( "StoredDataManager/target/test.db");
         writer.write( fos);
         fos.close();
     }
