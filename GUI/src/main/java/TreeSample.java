92c049599219fd32e7161a43d624c766b62e1554
aljose <JosAlberto@192.168.1.102>
2015-09-23 19:57:37 -0600

    GUI interfaz de usuario, LineNumber número de lineas en el editor, treeSample despliegue del árbol

---
+++ GUI/src/main/java/TreeSample.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author JoséAlberto
+ */
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+
+
+public class TreeSample {
+    JTree tree;
+  public TreeSample() {
+    
+    DefaultMutableTreeNode root = new DefaultMutableTreeNode("Bases de Datos");
+    DefaultTreeModel modelo = new DefaultTreeModel(root);
+    tree = new JTree(modelo);
+    
+    DefaultMutableTreeNode tabla = new DefaultMutableTreeNode("tabla");
+    DefaultMutableTreeNode venus = new DefaultMutableTreeNode("Venus");
+    modelo.insertNodeInto(tabla, root, 0);
+    modelo.insertNodeInto(venus, root, 1);
+    DefaultMutableTreeNode mars = new DefaultMutableTreeNode("Mars");
+    DefaultMutableTreeNode marsa = new DefaultMutableTreeNode("Marsa");
+    tabla.add(mars);
+    tabla.add(marsa);
+    
+  }
+}
