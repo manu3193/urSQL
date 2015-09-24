b03ca497e2f6d22043850662963d11e7392e63d6
manu3193 <manu_z3@hotmail.com>
2015-09-23 19:40:26 -0600

    Creada clase StoredDataManager

---
+++ StoredDataManager/src/main/java/StoredDataManager.java
@@ -0,0 +1,18 @@
+/**
+ * Created by manzumbado on 22/09/15.
+ */
+
+import java.util.ArrayList;
+
+/**
+ * Esta clase es la encargada de mantener las instancias de los arboles
+ * que pertenecen a la base de datos
+ */
+public class StoredDataManager {
+
+
+    private ArrayList mListaArboles;
+    private String mCurrentDataBase;
+
+
+}
