66a59ff9745d4e0c12aab417e19d0e6a074563c9
majesco <nicolas.j2007@gmail.com>
2015-09-27 02:20:41 -0600

    Reversiones en el lexical

--- src/main/java/DatabaseRuntimeProcessor/Analysis/LexicalAnalysis.java
+++ src/main/java/DatabaseRuntimeProcessor/Analysis/LexicalAnalysis.java
@@ -14,11 +14,11 @@ import java.util.StringTokenizer;
  */
 public class LexicalAnalysis {
 
-    private final ArrayList<ArrayList<String>> instructions;
+    private final ArrayList<String> instructions;
 
     public LexicalAnalysis() {
 
-        this.instructions = new ArrayList<ArrayList<String>> ();
+        this.instructions = new ArrayList<String> ();
     }
 
     /**
@@ -26,23 +26,15 @@ public class LexicalAnalysis {
      * @param instruction 
      * @return  
      */
-    public ArrayList<ArrayList<String>> tokenize(String instruction) {
+    public ArrayList<String> tokenize(String instruction) {
 
         StringTokenizer tokens = new StringTokenizer(instruction);
-        ArrayList<String> outputInstruction = new ArrayList();
 
         while (tokens.hasMoreElements()) {
 
             String elementoActual = tokens.nextToken();
 
-            if (elementoActual.equals(";")) {
-
-                instructions.add(outputInstruction);
-                outputInstruction.clear();
-
-            } else {
-                outputInstruction.add(tokens.nextToken());
-            }
+                instructions.add( elementoActual);
         }
         return instructions;
     }
--- src/main/java/DatabaseRuntimeProcessor/Analysis/MainParser.java
+++ src/main/java/DatabaseRuntimeProcessor/Analysis/MainParser.java
@@ -5,6 +5,8 @@
  */
 package DatabaseRuntimeProcessor.Analysis;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nicolasjimenez
@@ -16,7 +18,32 @@ public class MainParser {
      */
     public static void main(String[] args) {
 
-    
+        LexicalAnalysis lex = new LexicalAnalysis();
+
+//        ArrayList<ArrayList<String>> instrucciones = new ArrayList<ArrayList<String>>();
+//
+//        ArrayList<String> instruction1 = new ArrayList<String>();
+//        ArrayList<String> instruction2 = new ArrayList<String>();
+//        ArrayList<String> instruction3 = new ArrayList<String>();
+//
+//        instrucciones.add(instruction1);
+//        instrucciones.add(instruction2);
+//
+//        instruction1.add("get");
+//        instruction1.add("status");
+//        instruction1.add(";");
+//        
+//        instruction2.add("drop");
+//        instruction2.add("trffr");
+//        instruction2.add(";");
+
+        String temp = "get status ; "
+                + "drop database frfr ;"
+                + " create index on ; ";
+        
+        
+        lex.tokenize(temp);
+
     }
-    
+
 }
