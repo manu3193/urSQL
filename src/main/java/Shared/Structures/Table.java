823e6155fab9c0021c3ec843652a4a74185306db
kevuo <kevgiso@hotmail.com>
2015-09-27 01:45:30 -0600

    Se agregan atributos al objeto Table: nombre de la llave primaria, nombre de esquema al que pertenece, nombre de las columnas que la componen y sus respectivos setters y getters.

--- src/main/java/Shared/Structures/Table.java
+++ src/main/java/Shared/Structures/Table.java
@@ -3,24 +3,29 @@ package Shared.Structures;
 import java.util.ArrayList;
 
 /**
- * Clase encargada de crear un objeto Table el cual se genera cuando el usuario define una nueva tabla.
+ * Clase encargada de crear un objeto Table el cual se genera cuando el usuario
+ * define una nueva tabla.
+ *
  * @author Kevin
  */
 public class Table {
-    
+
     /*Atributos de clase los cuales describen el nombre de la tabla, un arreglo de las filas que componen la tabla y la
-      cantidad de filas existentes, respectivamente.*/
+     cantidad de filas existentes, respectivamente.*/
     private final String name;
     private ArrayList<Row> rows;
     private int length;
-    
-    public Table (String name){
+    private ArrayList<String> columnNames;
+    private String primaryKey;
+    private String schemaName;
+
+    public Table(String name) {
         this.name = name;
         this.rows = null;
         this.length = 0;
     }
-    
-    public Table (String name, ArrayList<Row> rows){
+
+    public Table(String name, ArrayList<Row> rows) {
         this.name = name;
         this.rows = rows;
         this.length = rows.size();
@@ -60,7 +65,47 @@ public class Table {
     public void setLength(int length) {
         this.length = length;
     }
-    
-    
-    
+
+    /**
+     * @return the columnNames
+     */
+    public ArrayList<String> getColumnNames() {
+        return columnNames;
+    }
+
+    /**
+     * @param columnNames the columnNames to set
+     */
+    public void setColumnNames(ArrayList<String> columnNames) {
+        this.columnNames = columnNames;
+    }
+
+    /**
+     * @return the primaryKey
+     */
+    public String getPrimaryKey() {
+        return primaryKey;
+    }
+
+    /**
+     * @param primaryKey the primaryKey to set
+     */
+    public void setPrimaryKey(String primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    /**
+     * @return the schemaName
+     */
+    public String getSchemaName() {
+        return schemaName;
+    }
+
+    /**
+     * @param schemaName the schemaName to set
+     */
+    public void setSchemaName(String schemaName) {
+        this.schemaName = schemaName;
+    }
+
 }
